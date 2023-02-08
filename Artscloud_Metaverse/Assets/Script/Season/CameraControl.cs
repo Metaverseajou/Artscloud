498b45e2acc47edf5587e6b8d0f6673e46ebfd5c
gaeunU <dbrkdmsyge12@naver.com>
2023-02-08 14:59:57 +0900

    캐릭터 움직임 수정 (방향회전은 아직 ~ ㅜ)

--- Artscloud_Metaverse/Assets/Script/Season/CameraControl.cs
+++ Artscloud_Metaverse/Assets/Script/Season/CameraControl.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    public Transform target;
+   [SerializeField]
+    float moveSpeed = 5.0f;
+    Vector3 moveDirection;
 
-    void Update(){
-        transform.LookAt(target);
+    private CharacterController characterController;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        moveDirection = new Vector3(x, 0, z);
+
+        characterController.Move(moveDirection * moveSpeed * Time.deltaTime);
     }
 }
